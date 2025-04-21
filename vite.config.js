f17e151f655676356d9f52c4d67a2b7c13616980
Medik3636 <Rasmiydorixona@gmail.com>
2025-04-21 17:50:15 +0500

    Updated vite.config.js to ensure correct resource loading

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Resurslar root dan yuklanishini ta'minlaydi
+  server: {
+    host: '0.0.0.0', // Barcha interfeyslardan kirishga ruxsat beradi
+    port: 5173,
+  },
 });
