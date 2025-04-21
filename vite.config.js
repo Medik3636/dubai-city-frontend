f5a25e36de50ea00b8c0db8fcc1c60ff22c3431c
Medik3636 <Rasmiydorixona@gmail.com>
2025-04-21 13:34:58 +0500

    Fresh start: Reinitialized frontend for GitHub deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Render’da to‘g‘ri ishlashi uchun
+  base: '/',
   build: {
-    outDir: 'dist', // Build natijasi dist papkasida bo‘lishi kerak
+    outDir: 'dist',
   },
 });
